42c233c77fa435c63598e120f4d6fbef2742f7cd
tmcgroul <safonov.a.r@mail.ru>
2023-07-27 18:34:59 +0300

    minimal implementation of blocks streaming api

--- src/main.rs
+++ src/main.rs
@@ -21,15 +21,13 @@ pub mod archive {
     use serde::{Serialize, Deserialize};
     use serde_json::json;
 
-    struct BatchRequest {}
-
     #[derive(Serialize, Deserialize, Debug)]
     #[serde(rename_all = "camelCase")]
     pub struct BlockHeader {
-        pub number: u32,
+        pub number: u64,
         pub hash: String,
         pub parent_hash: String,
-        pub size: u32,
+        pub size: u64,
         pub sha3_uncles: String,
         pub miner: String,
         pub state_root: String,
@@ -63,7 +61,7 @@ pub mod archive {
             Archive { client }
         }
 
-        pub async fn height(&self) -> Result<u32, reqwest::Error> {
+        pub async fn height(&self) -> Result<u64, reqwest::Error> {
             let height = self.client.get("https://v2.archive.subsquid.io/network/ethereum-mainnet/height")
                 .send()
                 .await?
@@ -74,42 +72,55 @@ pub mod archive {
             Ok(height)
         }
 
-        pub async fn query(&self, from_block: u64, to_block: u64) -> Result<Vec<Block>, reqwest::Error> {
+        pub async fn query(&self, from_block: u64, to_block: Option<u64>) -> Result<Vec<Block>, reqwest::Error> {
             let worker_url = self.client.get(format!("https://v2.archive.subsquid.io/network/ethereum-mainnet/{}/worker", from_block))
                 .send()
                 .await?
                 .text()
                 .await?;
-            let blocks: Vec<Block> = self.client.post(worker_url)
-                .json(&json!({
-                    "fromBlock": from_block,
-                    "toBlock": to_block,
-                    "fields": {
-                        "block": {
-                            "number": true,
-                            "hash": true,
-                            "parentHash": true,
-                            "difficulty": true,
-                            "totalDifficulty": true,
-                            "size": true,
-                            "sha3Uncles": true,
-                            "gasLimit": true,
-                            "gasUsed": true,
-                            "timestamp": true,
-                            "miner": true,
-                            "stateRoot": true,
-                            "transactionsRoot": true,
-                            "receiptsRoot": true,
-                            "logsBloom": true,
-                            "extraData": true,
-                            "mixHash": true,
-                            "baseFeePerGas": true,
-                            "nonce": true
-                        }
+
+            let mut query = json!({
+                "fromBlock": from_block,
+                "fields": {
+                    "block": {
+                        "number": true,
+                        "hash": true,
+                        "parentHash": true,
+                        "difficulty": true,
+                        "totalDifficulty": true,
+                        "size": true,
+                        "sha3Uncles": true,
+                        "gasLimit": true,
+                        "gasUsed": true,
+                        "timestamp": true,
+                        "miner": true,
+                        "stateRoot": true,
+                        "transactionsRoot": true,
+                        "receiptsRoot": true,
+                        "logsBloom": true,
+                        "extraData": true,
+                        "mixHash": true,
+                        "baseFeePerGas": true,
+                        "nonce": true
                     }
-                }))
+                }
+            });
+            if let Some(to_block) = to_block {
+                query.as_object_mut().unwrap().insert("toBlock".to_string(), to_block.into());
+            }
+
+            let response = self.client.post(worker_url)
+                .json(&query)
                 .send()
-                .await?
+                .await?;
+
+            if let Err(err) = response.error_for_status_ref() {
+                let text = response.text().await?;
+                dbg!(text);
+                return Err(err)
+            }
+
+            let blocks: Vec<Block> = response
                 .json()
                 .await?;
             Ok(blocks)
@@ -119,9 +130,9 @@ pub mod archive {
 
 async fn resolve_negative_start_block_num(start_block_num: i64, archive: &archive::Archive) -> u64 {
     if start_block_num < 0 {
-        let delta = u32::try_from(start_block_num.abs()).unwrap();
+        let delta = u64::try_from(start_block_num.abs()).unwrap();
         let head = archive.height().await.unwrap();
-        return head.saturating_sub(delta).into();
+        return head.saturating_sub(delta);
     }
     u64::try_from(start_block_num).unwrap()
 }
@@ -152,7 +163,14 @@ impl Stream for ArchiveStream {
             let filter = transforms::CombinedFilter::decode(&transform.value[..]).unwrap();
             filters.push(filter);
         }
-        let from_block = resolve_negative_start_block_num(request.get_ref().start_block_num, &self.archive).await;
+
+        let mut from_block = resolve_negative_start_block_num(request.get_ref().start_block_num, &self.archive).await;
+        let to_block = if request.get_ref().stop_block_num == 0 {
+            None
+        } else {
+            Some(request.get_ref().stop_block_num)
+        };
+
         dbg!(request.get_ref());
         dbg!(&filters);
         let (tx, rx) = tokio::sync::mpsc::channel(1);
@@ -160,15 +178,14 @@ impl Stream for ArchiveStream {
         tokio::spawn(async move {
             loop {
                 let height = archive.height().await.unwrap();
-                let blocks = archive.query(from_block, request.get_ref().stop_block_num).await.unwrap();
-                dbg!(&blocks);
+                let blocks = archive.query(from_block, to_block).await.unwrap();
                 let last_block_num = blocks[blocks.len() - 1].header.number;
                 for block in blocks {
                     let graph_block = Block {
                         ver: 2,
                         hash: prefix_hex::decode(block.header.hash.clone()).unwrap(),
-                        number: block.header.number.into(),
-                        size: block.header.size.into(),
+                        number: block.header.number,
+                        size: block.header.size,
                         header: Some(codec::BlockHeader {
                             parent_hash: prefix_hex::decode(block.header.parent_hash).unwrap(),
                             uncle_hash: prefix_hex::decode(block.header.sha3_uncles).unwrap(),
@@ -179,7 +196,7 @@ impl Stream for ArchiveStream {
                             logs_bloom: prefix_hex::decode(block.header.logs_bloom).unwrap(),
                             difficulty: Some(codec::BigInt { bytes: vec_from_hex(&block.header.difficulty).unwrap() }),
                             total_difficulty: Some(codec::BigInt { bytes: vec_from_hex(&block.header.total_difficulty).unwrap() }),
-                            number: block.header.number.into(),
+                            number: block.header.number,
                             gas_limit: u64::from_str_radix(&block.header.gas_limit.trim_start_matches("0x"), 16).unwrap(),
                             gas_used: u64::from_str_radix(&block.header.gas_used.trim_start_matches("0x"), 16).unwrap(),
                             timestamp: Some(prost_types::Timestamp {
@@ -191,7 +208,7 @@ impl Stream for ArchiveStream {
                             nonce: u64::from_str_radix(&block.header.nonce.trim_start_matches("0x"), 16).unwrap(),
                             hash: prefix_hex::decode(block.header.hash).unwrap(),
                             base_fee_per_gas: block.header.base_fee_per_gas
-                                .and_then(|val| Some(codec::BigInt { bytes: prefix_hex::decode(val).unwrap() })),
+                                .and_then(|val| Some(codec::BigInt { bytes: vec_from_hex(&val).unwrap() })),
                         }),
                         uncles: vec![],
                         transaction_traces: vec![],
@@ -204,13 +221,21 @@ impl Stream for ArchiveStream {
                             value: graph_block.encode_to_vec(),
                         }),
                         step: 1,
-                        cursor: "cursor".to_string()
+                        cursor: request.get_ref().cursor.clone(),
                     })).await.unwrap();
+                }
+
+                if let Some(to_block) = to_block {
+                    if u64::from(last_block_num) == to_block {
+                        break;
+                    }
                 }
 
                 if height <= last_block_num {
                     tokio::time::sleep(std::time::Duration::from_secs(5)).await;
                 }
+
+                from_block = last_block_num.into();
             }
         });
         Ok(Response::new(tokio_stream::wrappers::ReceiverStream::new(rx)))
