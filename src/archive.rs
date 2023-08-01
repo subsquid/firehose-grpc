c8d361da2962959cac3f22568f5d7dcd3f292f4d
tmcgroul <safonov.a.r@mail.ru>
2023-08-01 17:06:22 +0300

    setup minimal requirements for indexing subgraphs

--- src/archive.rs
+++ src/archive.rs
@@ -1,5 +1,95 @@
 use serde::{Serialize, Deserialize};
-use serde_json::json;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct LogRequest {
+    pub address: Vec<String>,
+    pub topic0: Vec<String>,
+    pub topic1: Vec<String>,
+    pub topic2: Vec<String>,
+    pub topic3: Vec<String>,
+    pub transaction: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct BlockFieldSelection {
+    pub number: bool,
+    pub hash: bool,
+    pub parent_hash: bool,
+    pub difficulty: bool,
+    pub total_difficulty: bool,
+    pub size: bool,
+    pub sha3_uncles: bool,
+    pub gas_limit: bool,
+    pub gas_used: bool,
+    pub timestamp: bool,
+    pub miner: bool,
+    pub state_root: bool,
+    pub transactions_root: bool,
+    pub receipts_root: bool,
+    pub logs_bloom: bool,
+    pub extra_data: bool,
+    pub mix_hash: bool,
+    pub base_fee_per_gas: bool,
+    pub nonce: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct LogFieldSelection {
+    pub log_index: bool,
+    pub transaction_index: bool,
+    pub address: bool,
+    pub data: bool,
+    pub topics: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct TxFieldSelection {
+    pub transaction_index: bool,
+    pub hash: bool,
+    pub nonce: bool,
+    pub from: bool,
+    pub to: bool,
+    pub input: bool,
+    pub value: bool,
+    pub gas: bool,
+    pub gas_price: bool,
+    pub max_fee_per_gas: bool,
+    pub max_priority_fee_per_gas: bool,
+    pub v: bool,
+    pub r: bool,
+    pub s: bool,
+    pub y_parity: bool,
+    pub gas_used: bool,
+    pub cumulative_gas_used: bool,
+    pub effective_gas_price: bool,
+    pub r#type: bool,
+    pub status: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct FieldSelection {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub block: Option<BlockFieldSelection>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub log: Option<LogFieldSelection>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub transaction: Option<TxFieldSelection>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct BatchRequest {
+    pub from_block: u64,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub to_block: Option<u64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub fields: Option<FieldSelection>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub logs: Option<Vec<LogRequest>>,
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
@@ -26,8 +116,45 @@ pub struct BlockHeader {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Log {
+    pub address: String,
+    pub data: String,
+    pub topics: Vec<String>,
+    pub log_index: u32,
+    pub transaction_index: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Transaction {
+    pub transaction_index: u32,
+    pub hash: String,
+    pub nonce: u64,
+    pub from: String,
+    pub to: String,
+    pub input: String,
+    pub value: String,
+    pub gas: String,
+    pub gas_price: String,
+    pub max_fee_per_gas: Option<String>,
+    pub max_priority_fee_per_gas: Option<String>,
+    pub v: String,
+    pub r: String,
+    pub s: String,
+    pub y_parity: Option<u8>,
+    pub gas_used: String,
+    pub cumulative_gas_used: String,
+    pub effective_gas_price: String,
+    pub r#type: i32,
+    pub status: i32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Block {
     pub header: BlockHeader,
+    pub logs: Option<Vec<Log>>,
+    pub transactions: Option<Vec<Transaction>>
 }
 
 #[derive(Debug)]
@@ -52,45 +179,8 @@ impl Archive {
         Ok(height)
     }
 
-    pub async fn query(&self, from_block: u64, to_block: Option<u64>) -> Result<Vec<Block>, reqwest::Error> {
-        let worker_url = self.client.get(format!("https://v2.archive.subsquid.io/network/ethereum-mainnet/{}/worker", from_block))
-            .send()
-            .await?
-            .text()
-            .await?;
-
-        let mut query = json!({
-            "fromBlock": from_block,
-            "fields": {
-                "block": {
-                    "number": true,
-                    "hash": true,
-                    "parentHash": true,
-                    "difficulty": true,
-                    "totalDifficulty": true,
-                    "size": true,
-                    "sha3Uncles": true,
-                    "gasLimit": true,
-                    "gasUsed": true,
-                    "timestamp": true,
-                    "miner": true,
-                    "stateRoot": true,
-                    "transactionsRoot": true,
-                    "receiptsRoot": true,
-                    "logsBloom": true,
-                    "extraData": true,
-                    "mixHash": true,
-                    "baseFeePerGas": true,
-                    "nonce": true
-                }
-            }
-        });
-        if let Some(to_block) = to_block {
-            query.as_object_mut().unwrap().insert("toBlock".to_string(), to_block.into());
-        }
-
-        let response = self.client.post(worker_url)
-            .json(&query)
+    pub async fn query(&self, request: &BatchRequest) -> Result<Vec<Block>, reqwest::Error> {
+        let response = self.client.get(format!("https://v2.archive.subsquid.io/network/ethereum-mainnet/{}/worker", request.from_block))
             .send()
             .await?;
 
@@ -100,9 +190,28 @@ impl Archive {
             return Err(err)
         }
 
-        let blocks: Vec<Block> = response
-            .json()
+        let worker_url = response.text().await?;
+
+        let response = self.client.post(worker_url)
+            .json(&request)
+            .send()
             .await?;
+
+        if let Err(err) = response.error_for_status_ref() {
+            let text = response.text().await?;
+            dbg!(text);
+            return Err(err)
+        }
+
+        let text = response.text().await?;
+        let blocks = match serde_json::from_str(&text) {
+            Ok(blocks) => blocks,
+            Err(err) => {
+                dbg!(text, err);
+                panic!("decode error");
+            }
+        };
+
         Ok(blocks)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use tonic::{transport::Server, Request, Response, Status};
 use firehose::stream_server::{Stream, StreamServer};
-use codec::Block;
+use codec::{Block, Log, TransactionTrace, TransactionReceipt};
 use prost::Message;
-use archive::Archive;
+use archive::{Archive, BatchRequest};
 use std::sync::Arc;
 use tokio_stream::wrappers::ReceiverStream;
 use std::time::Duration;
@@ -56,30 +56,103 @@ impl Stream for ArchiveStream {
     type BlocksStream = ReceiverStream<Result<firehose::Response, Status>>;
 
     async fn blocks(&self, request: Request<firehose::Request>) -> Result<Response<Self::BlocksStream>, Status> {
-        let mut filters = vec![];
+        let mut fields = archive::FieldSelection {
+            block: Some(archive::BlockFieldSelection {
+                base_fee_per_gas: true,
+                difficulty: true,
+                total_difficulty: true,
+                extra_data: true,
+                gas_limit: true,
+                gas_used: true,
+                hash: true,
+                logs_bloom: true,
+                miner: true,
+                mix_hash: true,
+                nonce: true,
+                number: true,
+                parent_hash: true,
+                receipts_root: true,
+                sha3_uncles: true,
+                size: true,
+                state_root: true,
+                timestamp: true,
+                transactions_root: true,
+            }),
+            log: None,
+            transaction: None,
+        };
+        let mut logs: Vec<archive::LogRequest> = vec![];
+
         for transform in &request.get_ref().transforms {
             let filter = transforms::CombinedFilter::decode(&transform.value[..]).unwrap();
-            filters.push(filter);
+            for log_filter in filter.log_filters {
+                let log_request = archive::LogRequest {
+                    address: log_filter.addresses.into_iter().map(|address| prefix_hex::encode(address)).collect(),
+                    topic0: log_filter.event_signatures.into_iter().map(|signature| prefix_hex::encode(signature)).collect(),
+                    topic1: vec![],
+                    topic2: vec![],
+                    topic3: vec![],
+                    transaction: true,
+                };
+                logs.push(log_request);
+                fields.log = Some(archive::LogFieldSelection {
+                    address: true,
+                    data: true,
+                    log_index: true,
+                    topics: true,
+                    transaction_index: true,
+                });
+                fields.transaction = Some(archive::TxFieldSelection {
+                    cumulative_gas_used: true,
+                    effective_gas_price: true,
+                    from: true,
+                    gas: true,
+                    gas_price: true,
+                    gas_used: true,
+                    input: true,
+                    max_fee_per_gas: true,
+                    max_priority_fee_per_gas: true,
+                    nonce: true,
+                    r: true,
+                    s: true,
+                    hash: true,
+                    status: true,
+                    to: true,
+                    transaction_index: true,
+                    r#type: true,
+                    v: true,
+                    value: true,
+                    y_parity: true,
+                })
+            }
         }
 
-        let mut from_block = resolve_negative_start_block_num(request.get_ref().start_block_num, &self.archive).await;
+        let is_head_stream = request.get_ref().start_block_num == -1;
+        let from_block = resolve_negative_start_block_num(request.get_ref().start_block_num, &self.archive).await;
         let to_block = if request.get_ref().stop_block_num == 0 {
             None
         } else {
             Some(request.get_ref().stop_block_num)
         };
-
+        let mut req = BatchRequest {
+            from_block,
+            to_block,
+            fields: Some(fields),
+            logs: Some(logs),
+        };
+        let mut request_count = 0;
         dbg!(request.get_ref());
-        dbg!(&filters);
+        dbg!(&req);
         let (tx, rx) = tokio::sync::mpsc::channel(1);
         let archive = self.archive.clone();
         tokio::spawn(async move {
-            loop {
+            'outer: loop {
+                request_count += 1;
                 let height = archive.height().await.unwrap();
-                let blocks = archive.query(from_block, to_block).await.unwrap();
+                let blocks = archive.query(&req).await.unwrap();
                 let last_block_num = blocks[blocks.len() - 1].header.number;
                 for block in blocks {
-                    let graph_block = Block {
+                    let mut graph_block = Block {
                         ver: 2,
                         hash: prefix_hex::decode(block.header.hash.clone()).unwrap(),
                         number: block.header.number,
@@ -113,14 +186,86 @@ impl Stream for ArchiveStream {
                         balance_changes: vec![],
                         code_changes: vec![],
                     };
-                    tx.send(Ok(firehose::Response {
+
+                    let mut logs_by_tx: std::collections::HashMap<u32, Vec<archive::Log>> = std::collections::HashMap::new();
+                    if let Some(logs) = block.logs {
+                        for log in logs {
+                            if logs_by_tx.contains_key(&log.transaction_index) {
+                                logs_by_tx.get_mut(&log.transaction_index).unwrap().push(log);
+                            } else {
+                                logs_by_tx.insert(log.transaction_index, vec![log]);
+                            }
+                        }
+                    }
+
+                    if let Some(transactions) = block.transactions {
+                        graph_block.transaction_traces = transactions.into_iter().map(|tx| {
+                            let logs = logs_by_tx.remove(&tx.transaction_index).unwrap_or_default().into_iter().map(|log| Log {
+                                address: prefix_hex::decode(log.address).unwrap(),
+                                data: prefix_hex::decode(log.data).unwrap(),
+                                block_index: log.log_index,
+                                topics: log.topics.into_iter().map(|topic| prefix_hex::decode(topic).unwrap()).collect(),
+                                index: log.transaction_index,
+                                ordinal: 0,
+                            }).collect();
+                            TransactionTrace {
+                                to: prefix_hex::decode(tx.to).unwrap(),
+                                nonce: tx.nonce,
+                                gas_price: Some(codec::BigInt { bytes: vec_from_hex(&tx.gas_price).unwrap() }),
+                                gas_limit: u64::from_str_radix(&tx.gas.trim_start_matches("0x"), 16).unwrap(),
+                                gas_used: u64::from_str_radix(&tx.gas_used.trim_start_matches("0x"), 16).unwrap(),
+                                value: Some(codec::BigInt { bytes: vec_from_hex(&tx.value).unwrap() }),
+                                input: prefix_hex::decode(tx.input).unwrap(),
+                                v: vec_from_hex(&tx.v).unwrap(),
+                                r: vec_from_hex(&tx.r).unwrap(),
+                                s: vec_from_hex(&tx.s).unwrap(),
+                                r#type: tx.r#type,
+                                access_list: vec![],
+                                max_fee_per_gas: tx.max_fee_per_gas
+                                    .and_then(|val| Some(codec::BigInt { bytes: vec_from_hex(&val).unwrap() })),
+                                max_priority_fee_per_gas: tx.max_priority_fee_per_gas
+                                    .and_then(|val| Some(codec::BigInt { bytes: vec_from_hex(&val).unwrap() })),
+                                index: tx.transaction_index,
+                                hash: prefix_hex::decode(tx.hash).unwrap(),
+                                from: prefix_hex::decode(tx.from).unwrap(),
+                                return_data: vec![],
+                                public_key: vec![],
+                                begin_ordinal: 0,
+                                end_ordinal: 0,
+                                status: tx.status,
+                                receipt: Some(TransactionReceipt {
+                                    state_root: vec![],
+                                    cumulative_gas_used: u64::from_str_radix(&tx.cumulative_gas_used.trim_start_matches("0x"), 16).unwrap(),
+                                    logs_bloom: prefix_hex::decode("0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000").unwrap(),
+                                    logs,
+                                }),
+                                calls: vec![],
+                            }
+                        })
+                        .collect()
+                    }
+
+                    if !is_head_stream {
+                        dbg!(format!(
+                            "sending №{}, from: {} to: {} count: {}",
+                            block.header.number,
+                            request.get_ref().start_block_num,
+                            request.get_ref().stop_block_num,
+                            request_count,
+                        ));
+                    }
+                    let result = tx.send(Ok(firehose::Response {
                         block: Some(prost_types::Any {
                             type_url: "type.googleapis.com/sf.ethereum.type.v2.Block".to_string(),
                             value: graph_block.encode_to_vec(),
                         }),
                         step: 1,
                         cursor: request.get_ref().cursor.clone(),
-                    })).await.unwrap();
+                    })).await;
+
+                    if let Err(_) = result {
+                        break 'outer;
+                    }
                 }
 
                 if let Some(to_block) = to_block {
@@ -131,9 +276,9 @@ impl Stream for ArchiveStream {
 
                 if height <= last_block_num {
                     tokio::time::sleep(Duration::from_secs(5)).await;
+                } else {
+                    req.from_block = last_block_num + 1;
                 }
-
-                from_block = last_block_num.into();
             }
         });
         Ok(Response::new(ReceiverStream::new(rx)))
